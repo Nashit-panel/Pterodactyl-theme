467873de20cda9940cb96a9fcd8c02c656671836
Nashit <159161842+Nashit-panel@users.noreply.github.com>
2024-09-10 12:57:40 +0600

    Update arix.sh

--- arix.sh
+++ arix.sh
@@ -9,9 +9,7 @@ echo "Running bash to load NVM..."
 bash
 
 # Change to the Pterodactyl directory
-echo "Navigating to /var/www/pterodactyl..."
-cd /var/www/pterodactyl || { echo "Directory /var/www/pterodactyl does not exist."; exit 1; }
-
+cd /var/www/pterodactyl
 # Install Node.js version 16 using NVM
 echo "Installing Node.js version 16..."
 nvm install 16
@@ -25,9 +23,7 @@ echo "Running Yarn..."
 yarn
 
 # Go back to /var/www directory
-echo "Navigating to /var/www..."
-cd /var/www || { echo "Directory /var/www does not exist."; exit 1; }
-
+cd /var/www
 # Download the Arix theme zip file
 echo "Downloading Arix theme..."
 wget https://github.com/Nashit-panel/Pterodactyl-theme/raw/main/arix%20v1.2.zip
@@ -37,9 +33,7 @@ echo "Unzipping Arix theme..."
 unzip 'arix v1.2.zip'
 
 # Change back to the Pterodactyl directory
-echo "Navigating back to /var/www/pterodactyl..."
-cd /var/www/pterodactyl || { echo "Directory /var/www/pterodactyl does not exist."; exit 1; }
-
+cd /var/www/pterodactyl
 # Run the Arix installation via Artisan
 echo "Running php artisan arix insta..."
 php artisan arix install
